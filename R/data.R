904e87462e218fbdeb682540671aabd3115a4607
Jason Roos <jason@gronkle.local>
2022-08-23 09:23:51 +0200

    Updated for 2022/23

--- R/data.R
+++ R/data.R
@@ -18,15 +18,19 @@ get_ECI_data <- function(name = NULL) {
 		utils::data('P2_satisfaction', package = 'ECICourse', envir = e)
 		assign('d', e$P2_satisfaction, envir = e)
 	}
+	if (name == 'problem-set-3-gift') {
+		utils::data('P3_gift', package = 'ECICourse', envir = e)
+		assign('d', e$P3_gift, envir = e)
+	}
 	if (name == 'module-6-tutorial') {
 		utils::data('M6_coupon', package = 'ECICourse',  envir =  e)
 		assign('d', e$M6_coupon, envir = e)
 	}
-	if (name == 'module-7-tutorial') {
+	if (name == 'module-5-tutorial') {
 		utils::data('M7_soda_prices', package = 'ECICourse', envir  = e)
 		assign('d', e$M7_soda_prices, envir = e)
 	}
-	if (name == 'module-8-tutorial') {
+	if (name == 'module-7-tutorial') {
 		utils::data('M8_loyalty', package = 'ECICourse', envir = e)
 		assign('d', e$M8_loyalty, envir = e)
 	}
